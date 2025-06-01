0b8f0db2c4b7b1bb5456725e2196550d7b185806
Andy Chapman <achapm87@gmail.com>
2025-06-01 02:08:09 -0400

    Deploy prep

--- medusa-config.ts
+++ medusa-config.ts
@@ -12,6 +12,15 @@ module.exports = defineConfig({
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     },
+    workerMode: process.env.MEDUSA_WORKER_MODE as
+      | "shared"
+      | "worker"
+      | "server",
+    redisUrl: process.env.REDIS_URL,
+  },
+  admin: {
+    disable: process.env.DISABLE_MEDUSA_ADMIN === "true" || false,
+    backendUrl: process.env.MEDUSA_BACKEND_URL || "http://localhost:9000",
   },
   modules: [
     {
@@ -56,6 +65,26 @@ module.exports = defineConfig({
           process.env.SANITY_STUDIO_URL || "http://localhost:3000/studio",
         type_map: {
           product: "product",
+        },
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/cache-redis",
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/event-bus-redis",
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/workflow-engine-redis",
+      options: {
+        redis: {
+          url: process.env.REDIS_URL,
         },
       },
     },
